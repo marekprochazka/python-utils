b3493ccbdad84da977ed7b1ffae52b0a7ed37ae8
Marek Prochazka <m.prochazka2002@gmail.com>
2022-05-15 09:38:58 +0200

    project path moved to batch file

--- execute.bat
+++ execute.bat
@@ -1,1 +1,5 @@
-python C:\Veci\programming\python-utils\python-utils.py "C:\Veci\programming\python-utils"
+@echo off
+
+set PROJECT_PATH="C:\Veci\programming\python-utils"
+
+python %PROJECT_PATH%\python-utils.py %PROJECT_PATH%
