a8e5a11adc44040eb662dc86a5fd563cc34fac21
Marek Prochazka <m.prochazka2002@gmail.com>
2022-05-20 15:44:18 +0200

    execute with venv

--- execute.bat
+++ execute.bat
@@ -1,5 +1,8 @@
 @echo off
-
 set PROJECT_PATH="C:\Veci\programming\python-utils"
 
+call %PROJECT_PATH%\venv\Scripts\activate
+
 python %PROJECT_PATH%\python-utils.py %PROJECT_PATH%
+
+call deactivate
