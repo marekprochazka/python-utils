fe9fbb4a6e47b1827d1a9599b89653aa4b946433
Marek Prochazka <m.prochazka2002@gmail.com>
2022-10-21 16:54:56 +0200

    #17: (default project path)

--- bin/pwu.bat
+++ bin/pwu.bat
@@ -1,6 +1,7 @@
 @echo off
-set PROJECT_PATH="C:\Veci\programming\python-utils"
-
+:: Change this to the path where you installed python-windows-utils
+set PROJECT_PATH="C:\python-windows-utils"
+:: ---------------------------------------------------------------
 call %PROJECT_PATH%\venv\Scripts\activate
 
 python %PROJECT_PATH%\python-utils.py %PROJECT_PATH%
